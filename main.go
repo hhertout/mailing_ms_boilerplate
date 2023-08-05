7bc3f02dac80c000b229ce7a9b13d1677e462bf7
hhertout <hugues.hertout@gmail.com>
2023-08-05 09:30:47 +0200

    adding dotenv package

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hhertout/go_mailing_ws.git/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := gin.Default()
 
-  router.Routes(r)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router.Routes(r)
+	r.Run()
 }
