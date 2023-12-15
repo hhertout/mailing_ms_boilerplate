e897c8355f826571ef0f64cda0773e58c4206e2f
hhertout <hugues.hertout@gmail.com>
2023-12-15 15:41:46 +0100

    test: adding test

---
+++ tests/main_test.go
@@ -0,0 +1,22 @@
+package tests_test
+
+import (
+	"mailer_ms/src/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPingRoute(t *testing.T) {
+	r := router.SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+
+	r.ServeHTTP(w, req)
+
+	t.Run("Expect status code is ok", func(t *testing.T) {
+		if w.Code != 200 {
+			t.Errorf("Invalid request")
+		}
+	})
+}
